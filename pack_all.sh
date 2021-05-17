01aa1c77e5e6efefebae41b1447d5c997c257abd
aceforeverd <teapot@aceforeverd.com>
2021-05-17 16:44:34 +0800

    build: do not upload zookeeper

--- pack_all.sh
+++ pack_all.sh
@@ -30,7 +30,7 @@ unpack_and_install libother
 tar czf "$INSTALL_DIR.tar.gz" "$INSTALL_DIR"
 
 mkdir -p "$SRC_DIR"
-tar xzf apache-zookeeper-*.tar.gz -C "$SRC_DIR"
+tar xzf src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
 tar czf "$SRC_DIR.tar.gz" "$SRC_DIR"
 
 popd
