bd75f55616a820756f9d84b789aec14e576d5af9
HuangWei <huangwei@apache.org>
2021-06-25 18:57:20 +0800

    fix building on mac

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hybridsql-asserts
 hybridsql packages and libraries
+
+fetch_resource.sh, then pack_xx.sh, finally pack_all.sh
--- pack_all.sh
+++ pack_all.sh
@@ -10,15 +10,25 @@ WORKDIR=${1:-artifact}
 INSTALL_DIR="thirdparty-$(date +%Y-%m-%d)"
 SRC_DIR="thirdsrc-$(date +%Y-%m-%d)"
 
+TARDIR=${2:-$WORKDIR}
+pushd $TARDIR
+TARDIR=$(pwd)
+echo "tars dir: $TARDIR"
+popd
+
 export OUT="$ROOT/$WORKDIR/$INSTALL_DIR"
 unpack_and_install() {
     local name=$1
-    find . -maxdepth 1 -type f -iname "$name*.tar.gz" -exec tar xzf {} \;
+    find $TARDIR -maxdepth 1 -type f -iname "$name*.tar.gz" -exec tar xzf {} \;
     pushd "$name"-*/
-    find . -type f -exec install -D {} "$OUT"/{} \;
+
+    # Mac install(1) doesn't support -D, so we create dirs first, then copy
+    find . -type d -exec install -d {} "$OUT"/{} \;
+    find . -type f -exec install {} "$OUT"/{} \;
     popd
 }
 
+mkdir -p $WORKDIR
 pushd "$WORKDIR"
 
 mkdir -p "$INSTALL_DIR"
@@ -30,7 +40,7 @@ unpack_and_install libother
 tar czf "$INSTALL_DIR.tar.gz" "$INSTALL_DIR"
 
 mkdir -p "$SRC_DIR"
-tar xzf src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
+tar xzf $TARDIR/src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
 tar czf "$SRC_DIR.tar.gz" "$SRC_DIR"
 
 popd
