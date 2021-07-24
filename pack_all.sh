cf2d04cb7539915404acef34046468c9ae265c2a
aceforeverd <teapot@aceforeverd.com>
2021-07-24 11:03:42 +0800

    fix

--- pack_all.sh
+++ pack_all.sh
@@ -22,7 +22,7 @@ SRC_DIR="thirdsrc-$(date +%Y-%m-%d)"
 export OUT="$ROOT/$WORKDIR/$INSTALL_DIR"
 unpack_and_install() {
     local name=$1
-    find "$TARDIR" -maxdepth 1 -type f -iname "$name*.tar.gz" -exec tar xzf {} -C "$OUT" --strip-components=1 \;
+    find . -maxdepth 1 -type f -iname "$name*.tar.gz" -exec tar xzf {} -C "$OUT" --strip-components=1 \;
 }
 
 mkdir -p "$OUT"
@@ -40,7 +40,7 @@ popd
 tar czf "$INSTALL_DIR.tar.gz" "$INSTALL_DIR"
 
 mkdir -p "$SRC_DIR"
-tar xzf "$TARDIR"/src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
+tar xzf src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
 tar czf "$SRC_DIR.tar.gz" "$SRC_DIR"
 
 popd
