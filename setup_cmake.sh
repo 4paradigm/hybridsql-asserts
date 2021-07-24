f678e7cb2d59b3108541927eba96c0b301c02b06
aceforeverd <teapot@aceforeverd.com>
2021-07-24 19:51:47 +0800

    fix

--- setup_cmake.sh
+++ setup_cmake.sh
@@ -7,8 +7,11 @@ INPUT=${1:-$(arch)}
 
 if [[ $INPUT = 'i386' || $INPUT = 'x86_64' || $INPUT = 'amd64' ]]; then
     ARCH=x86_64
-elif [[ $INPUT = 'aarch64' ]]; then
+elif [[ $INPUT = 'aarch64' || $INPUT = 'arm64' ]]; then
     ARCH=aarch64
+else
+    echo "Unsupported arch: $INPUT"
+    exit 1
 fi
 
 
