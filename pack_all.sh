45d31dcbf5869facce268426697c3481a6f91608
aceforeverd <teapot@aceforeverd.com>
2021-12-05 16:29:48 +0800

    add release job

--- pack_all.sh
+++ pack_all.sh
@@ -11,33 +11,19 @@ cd "$(dirname "$0")"
 
 source var.sh
 
-WORKDIR=${1:-.}
+INSTALL_PREFIX=${0:-deps}
+
 OS=$(os_type)
 ARCH=$(target_arch)
-
-ROOT=$(pwd)
 INSTALL_DIR="thirdparty-$(date +%Y-%m-%d)-$OS-$ARCH"
-SRC_DIR="thirdsrc-$(date +%Y-%m-%d)"
+# SRC_DIR="thirdsrc-$(date +%Y-%m-%d)"
 
-export OUT="$ROOT/$WORKDIR/$INSTALL_DIR"
-unpack_and_install() {
-    local name=$1
-    find . -maxdepth 1 -type f -iname "$name*.tar.gz" -exec tar xzf {} -C "$OUT" --strip-components=1 \;
-}
+pushd "$INSTALL_PREFIX"
+    mv usr "$INSTALL_DIR"
+    # needs a soft link lib64->lib for Mac OS
+    tar czf "$INSTALL_DIR.tar.gz" "$INSTALL_DIR"
 
-mkdir -p "$OUT"
-
-pushd "$WORKDIR"
-
-unpack_and_install boost
-unpack_and_install llvm
-unpack_and_install libother
-
-# needs a soft link lib64->lib for Mac OS
-tar czf "$INSTALL_DIR.tar.gz" "$INSTALL_DIR"
-
-mkdir -p "$SRC_DIR"
-tar xzf src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
-tar czf "$SRC_DIR.tar.gz" "$SRC_DIR"
-
+    # mv src "$SRC_DIR"
+    # tar xzf src/apache-zookeeper-*.tar.gz -C "$SRC_DIR"
+    # tar czf "$SRC_DIR.tar.gz" "$SRC_DIR"
 popd
